3d22a65faf931d08f1915e119a987600fe701cf5
mhahsler <michael@hahsler.net>
2016-08-03 13:47:48 -0500

    improved hullplot.

--- R/hullplot.R
+++ R/hullplot.R
@@ -1,25 +1,6 @@
-#######################################################################
-# dbscan - Density Based Clustering of Applications with Noise
-#          and Related Algorithms
-# Copyright (C) 2015 Michael Hahsler
-
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-
 hullplot <- function(x, cl, col = NULL,
-  cex = 0.5, lwd_hull = 2, main = "Convex Cluster Hulls", ...) {
+  cex = 0.5, hull_lwd = 1, hull_lty = 1, main = "Convex Cluster Hulls",
+    solid=FALSE, alpha = .2, ...) {
 
   if(is.list(cl)) cl <- cl$cluster
   if(!is.numeric(cl)) stop("could not get cluster assignment vector from cl.")
@@ -27,12 +8,23 @@ hullplot <- function(x, cl, col = NULL,
   if(is.null(col)) col <- palette()
   if(max(cl)+1L > length(col)) warning("not enought colors. some colors will be reused.")
 
+
   plot(x[,1:2], col = col[cl %% length(col) +1L], cex = cex, main = main, ...)
+
+  col_poly <- adjustcolor(col, alpha.f = alpha)
+  if(is.null(hull_lwd) || is.na(hull_lwd) || hull_lwd == 0) {
+    hull_lwd <- 1
+    border <- NA
+  }else border <- NULL
 
   for(i in 1:max(cl)) {
     d <- x[cl==i,]
     ch <- chull(d)
     ch <- c(ch, ch[1])
-    lines(d[ch,], col = col[i %% length(col) +1L], lwd=lwd_hull)
+    if(!solid)
+      lines(d[ch,], col = col[i %% length(col) +1L], lwd=hull_lwd, lty=hull_lty)
+    else
+      polygon(d[ch,], col = col_poly[i %% length(col_poly) +1L],
+        lwd=hull_lwd, lty=hull_lty, border = border)
   }
 }

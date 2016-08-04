d44fb02a97252ec84b1d58ee64135b7d70f9368e
mhahsler <michael@hahsler.net>
2016-08-04 15:36:32 -0500

    OPTICSXi plot now used omd to place lines for cluster below the plot.

--- R/optics.R
+++ R/optics.R
@@ -137,13 +137,23 @@ plot.optics <- function(x, y=NULL, cluster = TRUE, ...) {
         y_max <- max(x$reachdist[which(x$reachdist != Inf)])
         # Sort clusters by size
         hclusters <- x$clusters_xi[order(x$clusters_xi$end-x$clusters_xi$start),]
+
+        def.par <- par(no.readonly = TRUE)
+
+        # .1 means to leave 15% for the cluster lines
+        par(mar= c(2, 4, 4, 2) + 0.1, omd = c(0, 1, .15, 1))
+        y_increments <- (y_max/0.85*.15)/(nrow(hclusters)+1L)
+
         plot(x$reachdist[x$order], type="h", col=x$cluster[x$order]+1L,
              ylab = "Reachability dist.", xlab = NA, xaxt = "n",
              main = "Reachability Plot", yaxs="i", ylim=c(0,y_max), xaxt='n', ...)
-        y_increments <- ((y_max/(par("plt")[4]-par("plt")[3]))*(par("plt")[3]))/(2*nrow(hclusters))
+
         i <- 1:nrow(hclusters)
-        segments(x0=hclusters$start[i], y0=-(y_increments*i), x1=hclusters$end[i], col=hclusters$cluster_id[i]+1L, lwd=1, xpd=T)
-      }
+        segments(x0=hclusters$start[i], y0=-(y_increments*i),
+          x1=hclusters$end[i], col=hclusters$cluster_id[i]+1L, lwd=2, xpd=NA)
+        par(def.par)
+
+        }
     }else{
       plot(x$reachdist[x$order], type="h",
         ylab = "Reachability dist.", xlab = "OPTICS order",

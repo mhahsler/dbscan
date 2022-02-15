e133e90c9456ccf12676f74482f193847fabb62e
Michael Hahsler <michael@hahsler.net>
2022-02-15 10:31:55 -0600

    improved man page.

--- R/comps.R
+++ R/comps.R
@@ -17,7 +17,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-#' Find Connected Components in a NN Graph
+#' Find Connected Components in a Nearest-neighbor Graph
 #'
 #' Generic function and methods to find connected components in nearest neighbor graphs.
 #'

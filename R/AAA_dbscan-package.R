3415d7a989b47b53533c6a49a0dc9130eee7d6ba
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-06-21 21:11:29 +0200

    fix: missing matplot import from graphics (#63)

--- R/AAA_dbscan-package.R
+++ R/AAA_dbscan-package.R
@@ -9,7 +9,7 @@
 #' Hahsler M, Piekenbrock M, Doran D (2019). dbscan: Fast Density-Based Clustering with R. Journal of Statistical Software, 91(1), 1-30. \doi{10.18637/jss.v091.i01}
 #'
 #' @import Rcpp
-#' @importFrom graphics plot points lines text abline polygon par segments
+#' @importFrom graphics plot points lines text abline polygon par segments matplot
 #' @importFrom grDevices palette chull adjustcolor
 #' @importFrom stats dist hclust dendrapply as.dendrogram is.leaf prcomp
 #' @importFrom utils tail

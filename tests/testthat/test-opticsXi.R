55645ba05d02c16bb59d744c7f57e3f3528f1677
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-08-03 14:57:28 +0200

    tests: use recommended fixtures layout for test data

--- tests/testthat/test-opticsXi.R
+++ tests/testthat/test-opticsXi.R
@@ -1,12 +1,12 @@
 test_that("OPTICS-XI", {
-  load(system.file("test_data/test_data.rda", package = "dbscan"))
-  load(system.file("test_data/elki_optics.rda", package = "dbscan"))
-  load(system.file("test_data/elki_optics_xi.rda", package = "dbscan"))
+  load(test_path("fixtures", "test_data.rda"))
+  load(test_path("fixtures", "elki_optics.rda"))
+  load(test_path("fixtures", "elki_optics_xi.rda"))
 
   ### run OPTICS XI with parameters: xi=0.01, eps=1.0, minPts=5
   x <- test_data
   res <- optics(x, eps = 1.0,  minPts = 5)
-  res <- extractXi(res, xi=0.10, minimum=FALSE)
+  res <- extractXi(res, xi = 0.10, minimum = FALSE)
 
   ### Check to make sure ELKI results match R
   expected <- res$clusters_xi[, c("start", "end")]
